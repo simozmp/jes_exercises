d2568ba6ea2827e1369ab8d34e4a13c20ec06db9
simozmp96 <simo.zmp96@gmail.com>
2016-03-23 11:57:35 +0100

    solved issue of missing exchanges of stripes in 'vertical_stripes_exchange.py'

--- vertical_stripes_exchange.py
+++ vertical_stripes_exchange.py
@@ -6,38 +6,22 @@ def pixelExchange(sourcePixel, targetPixel) :
   setColor(targetPixel, getColor(sourcePixel))
   setColor(sourcePixel, color)
 
-#  NB. This function works only with a limited number of stripes
-#      (I assume that's because of the approximation of delta)
 
 def verticalStripesExchange(pic, stripesNumber) :
   # @param pic: Picture;
   # @param stripesNumber: int;    Number of stripes in the picture
   
   height = getHeight(pic)
-  delta = getWidth(pic) / stripesNumber    # Width of the stripes
-  
-  # print getWidth(pic)    # debug message
-  # print delta            # debug message
+  delta = float(getWidth(pic)) / float(stripesNumber)     # Width of the stripes (castings are done in order to
+                                                          # prevent missing exchanges of stripes in (*) cycle)
   
   for stripe in range(0, (stripesNumber/2)*2, 2) :        # "(stripesNumber/2)*2" is done in order to
                                                           # avoid an odd number of stripes (in case 
                                                           # of odd number of stripes, the last one
                                                           # will remain untouched)
-    for x in range(delta*stripe,(delta*stripe)+delta) :
-      # print String(x) + ", " + String(stripe)             # debug message
+    
+    for x in range(int(delta*stripe),int(delta*stripe)+int(delta)) :  # (*)
       for y in range(0,height) :
-        pixelExchange(getPixelAt(pic,x,y), getPixelAt(pic,x+delta,y))
+        pixelExchange(getPixelAt(pic,x,y), getPixelAt(pic,x+int(delta),y))
   
   return pic
-
-#---
-
-def floatToIntApprox(number) :
-  # @param number: float
-  
-  if number%1 <= 0.5 :
-    result = int(number)
-  else :
-    result = int(number) + 1
-  
-  return result
